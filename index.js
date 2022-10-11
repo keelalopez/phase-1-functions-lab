1cd6243140ff4db7c002de76f2953e658fa1ddbe
Keila Lopez <keilalofra@gmail.com>
2022-10-11 14:19:57 -0700

    commit tree 388a459db9db2fc9b81f7a7e8a7ee7120c135a29

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (pickUp) {
+    let numberOfBlocks = Math.abs(pickUp - 42);
+    return numberOfBlocks;
+  };
+
+function distanceFromHqInFeet (blocksToFeet) {
+  let blockDifferenceResult = distanceFromHqInBlocks(blocksToFeet);
+  let totalFeet = blockDifferenceResult * 264;
+  return totalFeet;
+};
+
+function distanceTravelledInFeet (start, destination) {
+    let absoluteDistance = Math.abs(destination - start)* 264; 
+    return absoluteDistance;
+};
+
+function calculatesFarePrice (start, destination) {
+    let totalFeet = distanceTravelledInFeet(start, destination);
+
+    if (totalFeet <= 400) return 0;
+    if (totalFeet > 400 && totalFeet <= 2000) return (totalFeet-400)*0.02;
+    if (totalFeet > 2000 && totalFeet < 2500) return 25;
+    return 'cannot travel that far';
+};
+
+
+
+  console.log(calculatesFarePrice(1, 10));
